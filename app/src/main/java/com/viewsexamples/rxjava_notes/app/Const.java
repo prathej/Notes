91a4c07ee7685573baf0c3db3af97d0710857eba
Prahlad V <prathej1990@gmail.com>
2019-03-12 00:50:43 +0530

    Source code added

---
+++ app/src/main/java/com/viewsexamples/rxjava_notes/app/Const.java
@@ -0,0 +1,5 @@
+package com.viewsexamples.rxjava_notes.app;
+
+public class Const {
+    public static final String BASE_URL = "https://demo.androidhive.info/";
+}
---
+++ app/src/main/java/com/viewsexamples/rxjava_notes/network/model/BaseResponse.java
@@ -0,0 +1,9 @@
+package com.viewsexamples.rxjava_notes.network.model;
+
+public class BaseResponse {
+    String error;
+
+    public String getError() {
+        return error;
+    }
+}
